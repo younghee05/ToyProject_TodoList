4c272ec040a6847fe4a6feac688fab704c9d5c4a
gurwls980 <gurwls980@gmail.com>
2024-08-02 10:31:33 +0900

    fixed: import issue resolved

--- todolist_front/src/components/ListContainer/ListContainer.jsx
+++ todolist_front/src/components/ListContainer/ListContainer.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
+import api from '../../apis/instance';
+import ReactModal from 'react-modal';
 
 function ListContainer({ todoList, getTodoList, title }) {
 
